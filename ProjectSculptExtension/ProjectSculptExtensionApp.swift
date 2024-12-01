1505267b87ac1563917d3a0bd433352ce6a2f745
Robert <rob76c@gmail.com>
2024-12-01 18:33:57 -0500

    Initial Commit

---
+++ ProjectSculptExtension/ProjectSculptExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectSculptExtensionApp.swift
+//  ProjectSculptExtension
+//
+//  Created by Monty on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSculptExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
